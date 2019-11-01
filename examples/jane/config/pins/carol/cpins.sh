1f5608732110427ace381d73035b309117d4f36e
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-10-31 22:29:38 -0400

    organizing

--- examples/jane/config/pins/carol/cpins.sh
+++
@@ -1,1 +0,0 @@
-python3 /coderoot/pins/scripts/pins-to-c/pins-to-c.py jane-io.json
---
+++ examples/jane/scripts/pin-config-to-c-code.sh
@@ -0,0 +1,1 @@
+python3 /coderoot/pins/scripts/pins-to-c.py /coderoot/pins/examples/jane/config/pins/carol/jane-io.json
