62a6824b5abcfccae7a8e0a56363b3652ff6d4b4
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2020-03-18 02:52:00 -0400

    Pin interrupts in ESP32/arduino, first test ok

--- code/arduino/pins_hw_defs.h
+++ code/arduino/pins_hw_defs.h
@@ -17,8 +17,12 @@
 /* Operating system/platform specific defines for interrupt handlers
  */
 #ifdef ESP_PLATFORM
-  typedef void IRAM_ATTR pin_interrupt_handler(void);
-  #define BEGIN_PIN_INTERRUPT_HANDLER(func_name) void IRAM_ATTR func_name() {
+  #ifdef PINS_OS_INT_HANDLER_HDRS
+  #include "esp_attr.h"
+  #endif
+  // typedef void IRAM_ATTR pin_interrupt_handler(void);
+  typedef void pin_interrupt_handler(void);
+  #define BEGIN_PIN_INTERRUPT_HANDLER(name) void IRAM_ATTR name() {
   #define END_PIN_INTERRUPT_HANDLER }
   #define PINS_SIMULATED_INTERRUPTS 0
 #endif
