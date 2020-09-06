4df213a22b36acf6b7919fd6cd1752ea2275e955
mint pekka <pekka.lehtikoski@gmail.com>
2020-09-05 20:26:15 -0400

    xx

--- extensions/devicebus/pigpio/pins_pigpio_devicebus.c
+++ extensions/devicebus/pigpio/pins_pigpio_devicebus.c
@@ -595,8 +595,9 @@ static long ulledoo; if (++ulledoo > 10009) {osal_debug_error("ulledoo SPI\n"); 
         {
             s = pins_bus_run_i2c(bus);
             if (s == OSAL_COMPLETED) {
-                os_timeslice();
+                os_sleep(20);
             }
+            os_timeslice();
 static long ulledoo; if (++ulledoo > 10009) {osal_debug_error("ulledoo I2C\n"); ulledoo = 0;}
         }
     }
